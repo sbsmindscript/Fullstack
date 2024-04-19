1161213183139a4fb92e05ede2884a6ad8bb351b
sbsmindscript <125560322+sbsmindscript@users.noreply.github.com>
2024-04-19 15:44:44 +0530

    ok
    ok

--- bank-pigmy-app-setup/bank_api/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *'); 
-
-define('DB_SERVER', 'localhost'); 
-define('DB_USERNAME', 'root'); 
-define('DB_PASSWORD', ''); 
-define('DB_DATABASE', 'bank_db');  
-$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE)or die("Error in Selecting " . mysqli_error($conn));
-?>
